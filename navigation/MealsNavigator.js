ad1f0a46f2aa08fec79207cd9988f54c8857fb14
Jack M <jackm550@googlemail.com>
2021-08-19 16:48:41 +0100

    added a working bottom tab navigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,5 +1,8 @@
+import React from "react"
 import { createStackNavigator } from "react-navigation-stack"
+import { createBottomTabNavigator } from "react-navigation-tabs"
 import { createAppContainer } from "react-navigation"
+import { Ionicons } from "@expo/vector-icons"
 
 import Colors from "../constants/colors"
 
@@ -7,6 +10,7 @@ import Colors from "../constants/colors"
 import CategoriesScreen from "../screens/CategoriesScreen"
 import CategoryMealsScreen from "../screens/CategoryMealsScreen"
 import MealDetailScreen from "../screens/MealDetailScreen"
+import FavoritesScreen from "../screens/FavoritesScreen"
 
 //navigator to point to screens
 const MealsNavigator = createStackNavigator(
@@ -27,5 +31,39 @@ const MealsNavigator = createStackNavigator(
   }
 )
 
+const MealsFavTabNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons
+              name='ios-restaurant'
+              size={25}
+              color={tabInfo.tintColor}
+            />
+          )
+        },
+      },
+    },
+    Favorites: {
+      screen: FavoritesScreen,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />
+          )
+        },
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accentColor,
+    },
+  }
+)
+
 //export navigator wrapped in an app container from react-navigation
-export default createAppContainer(MealsNavigator)
+export default createAppContainer(MealsFavTabNavigator)
